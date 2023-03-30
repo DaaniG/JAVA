bc856dcc899da6fd7de6de121c4b3ee7014826fe
Titodev <daniel.guardado@outlook.es>
2023-03-30 20:25:07 +0200

    Create EjemploMetodos.java

---
+++ Ruta de aprendizaje/4. Programación Orientada a Objetos/2. Métodos y funciones/EjemploMetodos.java
@@ -0,0 +1,19 @@
+package MetodosYFunciones;
+
+/**
+ * Métodos y funciones
+ * @author TitoDev
+ */
+
+public class EjemploMetodos {
+
+    public static void main(String[] args) {
+        int resultado = sumar(3, 4);
+        System.out.println(resultado);
+    }
+
+    public static int sumar(int num1, int num2) {
+        int resultado = num1 + num2;
+        return resultado;
+    }
+}
