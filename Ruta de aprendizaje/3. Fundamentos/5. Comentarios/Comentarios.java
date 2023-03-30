72c626ef241cd606a1a6af1342bff25394858280
Titodev <daniel.guardado@outlook.es>
2023-03-30 19:10:21 +0200

    Create Comentarios.java

---
+++ Ruta de aprendizaje/3. Fundamentos/5. Comentarios/Comentarios.java
@@ -0,0 +1,20 @@
+package Comentarios;
+
+/**
+ * Comentarios
+ * @author TitoDev
+ */
+
+public class Comentarios {
+    // Este es un comentario de una sola línea
+
+    /*
+    Este es un comentario de varias líneas.
+    Puede ser muy útil para explicar partes complejas del código.
+    */
+
+    /**
+     * Este es un comentario de documentación.
+     * Se utiliza para generar documentación del código.
+     */
+}
